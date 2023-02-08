415ccddfa4cbe5261cd68bc7cd50423ac61e4eca
Nguyễn Tiến Tùng <nttung.code@gmail.com>
2023-02-08 14:07:48 +0700

    them truong-fix order

--- lib/controllers/cart_controller.dart
+++ lib/controllers/cart_controller.dart
@@ -13,6 +13,7 @@ class CartController extends GetxController {
 
   late dynamic productSnapshot;
   var products = [];
+  var vendors = [];
 
   changePaymentIndex(index) {
     paymentIndex.value = index;
@@ -56,12 +57,14 @@ class CartController extends GetxController {
       'order_on_delivery' : false,
       'total_amount' : totalAmount,
       'orders' : FieldValue.arrayUnion(products),
+      'vendors': FieldValue.arrayUnion(vendors),
     });
     placingOrder(false);
   }
 
   getProductDetails() {
     products.clear();
+    vendors.clear();
     for ( var i = 0; i <  productSnapshot.length; i++) {
       products.add({
         'color' : productSnapshot[i]['color'],
@@ -71,6 +74,7 @@ class CartController extends GetxController {
         'qty' : productSnapshot[i]['qty'],
         'title' : productSnapshot[i]['title']
       });
+      vendors.add(productSnapshot[i]['vendor_id']);
     }
   }
 
